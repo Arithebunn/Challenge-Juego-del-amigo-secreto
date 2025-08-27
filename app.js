fd2fbb12c80af138b969d5641a28537085330952
Ari Andrea <ariadna.escobedo.c@gmail.com>
2025-08-26 22:59:48 -0600

    Agregar amigo a la lista invisible

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+
+//Al hacer click en añadir
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById('amigo').value;
+    listaDeAmigos.push(nuevoAmigo);
+    limpiarCaja();
+    
+    return console.log(nuevoAmigo);
+}
+
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
