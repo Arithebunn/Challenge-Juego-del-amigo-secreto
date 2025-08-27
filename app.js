c49f5c8aaa1f69e65bfffbcf9988bf6f847bb7c6
Ari Andrea <ariadna.escobedo.c@gmail.com>
2025-08-27 13:41:32 -0600

    Funcion Sortear amigo al hacer click

--- app.js
+++ app.js
@@ -6,16 +6,24 @@ let listaDeAmigos = [];
 function agregarAmigo() {
     let nuevoAmigo = document.getElementById('amigo').value;
     if (nuevoAmigo == '') {
-        alert('Inserte un nombre válido');
+        alert('Inserte un nombre válido'); //Validar entrada
     } else {
-        listaDeAmigos.push(nuevoAmigo);
+        listaDeAmigos.push(nuevoAmigo); //Agregar nombre a lista invisible
         limpiarCaja();
+        //document.querySelector('#listaAmigos').innerHTML = nuevoAmigo;
     }
     return console.log(nuevoAmigo);
 }
 
-
+//Al hacer click en Sortear amigo
+function sortearAmigo() {
+    let indiceAleatorio = Math.floor(Math.random()*listaDeAmigos.length);
+    console.log(indiceAleatorio);
+    console.log(listaDeAmigos[indiceAleatorio]);
+    return document.querySelector('#resultado').innerHTML = listaDeAmigos[indiceAleatorio];
+}
 
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
 }
+
